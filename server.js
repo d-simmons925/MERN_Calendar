94a03518c1d7ccc27f65863858fcdbe24e6eee97
d-simmons925 <42946974+d-simmons925@users.noreply.github.com>
2021-06-27 09:24:58 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const path = require('path')
 
 const app = express()
 
@@ -16,6 +17,13 @@ mongoose
 app.use('/events', require('./routes/events'))
 app.use('/users', require('./routes/users'))
 app.use('/auth', require('./routes/auth'))
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
