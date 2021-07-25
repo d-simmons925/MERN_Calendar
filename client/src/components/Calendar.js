f962b71d7e28cc20df149f73e4f6e367bd203190
d-simmons925 <42946974+d-simmons925@users.noreply.github.com>
2021-07-25 03:12:53 -0400

    adjusted css to support mobile devices

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,58 +1,55 @@
-import {useState, useEffect} from 'react'
-import {connect, useDispatch} from 'react-redux'
-import {getEvents, clearEvents} from '../actions/eventActions'
-import CalendarHeader from './CalendarHeader'
-import Day from './Day'
-import {useDate} from '../hooks/useDate'
+import { useState, useEffect } from "react";
+import { connect, useDispatch } from "react-redux";
+import { getEvents, clearEvents } from "../actions/eventActions";
+import CalendarHeader from "./CalendarHeader";
+import Day from "./Day";
+import { useDate } from "../hooks/useDate";
 
-const Calendar = ({events, isAuthenticated}) => {
-  const dispatch = useDispatch()
-  const [nav, setNav] = useState(0)
+const Calendar = ({ events, isAuthenticated }) => {
+  const dispatch = useDispatch();
+  const [nav, setNav] = useState(0);
 
   useEffect(() => {
-      if(isAuthenticated){
-        dispatch(getEvents())
-      }
-      if(!isAuthenticated){
-        dispatch(clearEvents())
-      }
-  },[isAuthenticated, dispatch])
+    if (isAuthenticated) {
+      dispatch(getEvents());
+    }
+    if (!isAuthenticated) {
+      dispatch(clearEvents());
+    }
+  }, [isAuthenticated, dispatch]);
 
-  const {days, dateDisplay} = useDate(events, nav)
+  const { days, dateDisplay } = useDate(events, nav);
 
   return (
     <div>
       <div id="container">
         <CalendarHeader
           dateDisplay={dateDisplay}
-          onBack={()=> setNav(nav - 1)}
-          onNext={()=> setNav(nav + 1)}
+          onBack={() => setNav(nav - 1)}
+          onNext={() => setNav(nav + 1)}
         />
-      <div id="weekdays">
-        <div>Sunday</div>
-        <div>Monday</div>
-        <div>Tuesday</div>
-        <div>Wednesday</div>
-        <div>Thursday</div>
-        <div>Friday</div>
-        <div>Saturday</div>
-      </div>
+        <div id="weekdays">
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tues</div>
+          <div>Wed</div>
+          <div>Thurs</div>
+          <div>Fri</div>
+          <div>Sat</div>
+        </div>
         <div id="calendar">
           {days.map((d, index) => (
-            <Day 
-              key={index}
-              day={d}
-            />
+            <Day key={index} day={d} />
           ))}
         </div>
-      </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({
-  events : state.event.events,
-  isAuthenticated: state.auth.isAuthenticated
-})
+const mapStateToProps = (state) => ({
+  events: state.event.events,
+  isAuthenticated: state.auth.isAuthenticated,
+});
 
-export default connect(mapStateToProps, null)(Calendar)
+export default connect(mapStateToProps, null)(Calendar);
